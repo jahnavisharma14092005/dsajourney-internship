dac9505ab6521f1e62b368193fa11d657ffd74cd
jahnavisharma14092005 <jahnaviprofess@gmail.com>
2026-06-26 22:14:35 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) 
+    {
+           
+           while(stones.size()>1)
+           { sort(stones.begin(),stones.end());
+            int x=stones.back();
+              stones.pop_back();
+              int y=stones.back();
+              stones.pop_back();
+              if(x!=y)
+              {
+                  stones.push_back(x-y);
+
+
+              }
+           }
+
+           return stones.empty() ? 0:stones[0];
+
+
+
+
+           
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+    }
+};
