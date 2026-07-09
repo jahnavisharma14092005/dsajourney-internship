5264334ba546a63b98feecb88b617e578e5626de
jahnavisharma14092005 <jahnaviprofess@gmail.com>
2026-07-09 20:44:09 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) 
+    {
+          unordered_map<char,int>mp;
+          {
+          for(auto p:s)
+          mp[p]++;
+          
+          unordered_map<char,int>mp1;
+          {
+          for(auto d:t)
+          mp1[d]++;
+          
+          if(mp!=mp1)
+          {
+          return false;
+          }
+          }
+          } 
+          return true;
+
+
+          
+
+
+
+
+
+
+
+
+        
+    }
+};
