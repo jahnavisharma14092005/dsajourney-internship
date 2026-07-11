0489de0c64c3f0496b2324f62f077f911e5a5bb3
jahnavisharma14092005 <jahnaviprofess@gmail.com>
2026-07-11 18:13:24 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+               if(s.length()!=t.length())
+               {
+                return false;
+               }
+                 int freq1[26]={0};
+                 int freq2[26]={0};
+           for(auto x:s)
+           {
+            
+            freq1[x-'a']++;
+
+
+           }
+           for(auto ch:t)
+           {
+            freq2[ch-'a']++;
+           }
+           for(int i=0;i<26;i++)
+           {
+            if(freq1[i]!=freq2[i])
+            return false;
+           }
+
+
+        
+                    return true;
+        
+    }
+};
